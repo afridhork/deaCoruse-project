278b7e56e64655a093ed37b57398e75c9ee752de
Afridho R Kartawiria <afridhorkartawiria@gmail.com>
2023-12-11 22:14:25 +0700

    update cart

--- src/features/cart/CartModal.jsx
+++ src/features/cart/CartModal.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import Modal from "../../components/Modal";
+import CartIcon from '../../assets/cartGray.svg'
 import {
   removeItemFromCart,
   selectCartItems,
@@ -76,70 +77,95 @@ const CartModal = ({ handleHideModalCart, totalItems, handleTotalItems }) => {
     <Modal>
       <div className="flex flex-col gap-6 p-1 sm:p-2 w-full lg:w-[400px] max-h-[500px]">
         <div className="flex flex-col gap-6 max-h-[500px] overflow-auto">
-          {products.map((product,index) => {
-            return (
-              <div
-                className="w-full border-b-4 border-blue-200 pb-4"
-                key={product.id}
-              >
-                <div className="flex items-center w-full">
-                  <div className="w-[120px] h-auto overflow-hidden">
-                    <img
-                      src={product.image}
-                      alt={product.title}
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-                  <div className="ml-10 w-[75%]">
-                    <h3 className="capitalize mt-3 text-lg">{product.title}</h3>
-                    <div className="flex items-center gap-2">
-                      <h4 className="text-sm">{product.price}</h4>
-                      <h3 className="text-lg font-bold">
-                        {product.totalPrice}
-                      </h3>
+          {
+            products.length > 0 ? (
+              products.map((product,index) => {
+                return (
+                  <div
+                    className="w-full border-b-4 border-blue-200 pb-4"
+                    key={product.id}
+                  >
+                    <div className="flex items-center w-full">
+                      <div className="w-[120px] h-auto overflow-hidden">
+                        <img
+                          src={product.image}
+                          alt={product.title}
+                          className="w-full h-full object-cover"
+                        />
+                      </div>
+                      <div className="ml-10 w-[75%]">
+                        <h3 className="capitalize mt-3 text-lg">{product.title}</h3>
+                        <div className="flex items-center gap-2">
+                          <h4 className="text-sm">{product.price}</h4>
+                          <h3 className="text-lg font-bold">
+                            {product.totalPrice}
+                          </h3>
+                        </div>
+                        <div className="flex items-center gap-4 mt-4 ml-auto">
+                          <button
+                            type="button"
+                            className="rounded-full bg-blue-400 w-5 h-5 text-white flex items-center justify-center"
+                            onClick={()=>decreaseQty(product,index)}
+                            >
+                            -
+                          </button>
+                          <h3>{product.quantity}</h3>
+                          <button
+                            type="button"
+                            className="rounded-full bg-blue-400 w-5 h-5 text-white flex items-center justify-center"
+                            onClick={()=>increaseQty(product)}
+                          >
+                            +
+                          </button>
+                        </div>
+                      </div>
                     </div>
-                    <div className="flex items-center gap-4 mt-4 ml-auto">
-                      <button
-                        type="button"
-                        className="rounded-full bg-blue-400 w-5 h-5 text-white flex items-center justify-center"
-                        onClick={()=>decreaseQty(product,index)}
-                        >
-                        -
-                      </button>
-                      <h3>{product.quantity}</h3>
-                      <button
-                        type="button"
-                        className="rounded-full bg-blue-400 w-5 h-5 text-white flex items-center justify-center"
-                        onClick={()=>increaseQty(product)}
-                      >
-                        +
-                      </button>
-                    </div>
                   </div>
-                </div>
+                );
+              })
+            ):(
+              <div className="text-center">
+                <h1>You have no product at cart</h1>
               </div>
-            );
-          })}
+            )
+          }
         </div>
         <div>
-          <h3 className="text-md font-bold">Total Item: {totalItems}</h3>
-          <h3 className="text-md font-bold">Total Price: {totalPriceState.toFixed(2)}</h3>
+          {
+            products.length > 0 ? (
+              <>
+                <h3 className="text-md font-bold">Total Item: {totalItems}</h3>
+                <h3 className="text-md font-bold">Total Price: {totalPriceState.toFixed(2)}</h3>
+              </>
+            ):(
+              <>
+                <div className="flex justify-center">
+                  <img src={CartIcon} alt="" className="w-12 h-12" />
+                </div>
+                <h4 className="text-lg text-center text-gray-300">Empty</h4>
+              </>
+            )
+          }
         </div>
-        <div className="flex items-center justify-between">
+        <div className={`flex items-center ${products.length > 0 ? 'justify-between' : 'justify-end'}`}>
           <button
             type="button"
-            className="bg-slate-600 hover:bg-slate-800 text-white py-3 px-8 rounded-xl text-sm"
+            className="bg-slate-600 hover:bg-slate-800 text-white py-3 px-8 rounded-xl text-sm mr-0 sm:mr-4"
             onClick={handleHideModalCart}
           >
             Close
           </button>
-          <button
-            type="button"
-            className="bg-green-600 hover:bg-slate-800 text-white font-bold py-3 px-8 rounded-xl text-sm"
-            onClick={handleCheckoutToWhatsapp}
-          >
-            Checkout (whatsapp)
-          </button>
+          {
+            products.length > 0 && (
+              <button
+                type="button"
+                className="bg-green-600 hover:bg-slate-800 text-white font-bold py-3 px-8 rounded-xl text-sm"
+                onClick={handleCheckoutToWhatsapp}
+              >
+                Checkout (whatsapp)
+              </button>
+            )
+          }
         </div>
       </div>
     </Modal>
